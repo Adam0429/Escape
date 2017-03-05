e62f6903020e80ad94efb122d24e2e800aa6b573
wfh0429 <872490934@qq.com>
2017-03-05 22:54:46 +0800

    2017.3.5
    可以通过门转换地图

---
+++ 课设/src/课设/MyKeyListener.java
@@ -0,0 +1,49 @@
+package 课设;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MyKeyListener implements KeyListener{
+	MainFrame frame;
+	hero h;
+	public MyKeyListener(MainFrame Frame,hero h1){
+		frame=Frame;
+		h=MainFrame.h;
+	}
+	public void keyPressed(KeyEvent e) {
+		 if(e.getKeyCode() == KeyEvent.VK_RIGHT) {  	
+			 h.right=true;
+		 }
+		 if(e.getKeyCode() == KeyEvent.VK_LEFT) {  
+		     h.left=true;	
+		 }
+		 if(e.getKeyCode() == KeyEvent.VK_SPACE) {  
+			 h.down=false;
+			 h.up=true;
+		 }
+		 if(e.getKeyCode() == KeyEvent.VK_ENTER) {  	
+			 frame.Current=frame.map1303;
+		 }
+		 if(e.getKeyCode() == KeyEvent.VK_E) {  				
+			 if(Trigger.enter(h))
+			 	 frame.Current=frame.mapStart;
+		 }
+	}
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {  
+			 h.right=false;	
+			 /*用这种写法会比用之前的写法手感好.原来的只有按下去一段时间才会走，猜想是多线程的缘故
+			  用多线程提高了人物的灵敏度。
+			  */
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {  
+			h.left=false;	
+		}
+		if(e.getKeyCode() == KeyEvent.VK_SPACE) {  
+			h.up=false;
+			h.down=true;	
+		}	 
+	}
+	public void keyTyped(KeyEvent e) {
+	}    
+}
---
+++ 课设/src/课设/MyMouseListener.java
@@ -0,0 +1,41 @@
+package ¿ÎÉè;
+
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MyMouseListener implements MouseListener{
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println(e.getX());
+		System.out.println(e.getY());
+		
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
